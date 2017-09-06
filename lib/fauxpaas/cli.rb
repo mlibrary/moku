fd90047fe194b33fb4b63b24a9279e3314e26a9a
Bryan Hockey <bhock@umich.edu>
2017-09-06 15:55:56 -0500

    var and file commands in cli

--- lib/fauxpaas/cli.rb
+++ lib/fauxpaas/cli.rb
@@ -1,36 +1,66 @@
 require "thor"
+require "fauxpaas"
 
 module Fauxpaas
   module CLI
 
     class Var < Thor
       desc "list <named_instance>", "List out the config"
-      def list(named_instance); end
+      def list(named_instance)
+        #TODO: authz
+        Instance.new(named_instance).var_file
+          .list
+      end
 
       desc "add <named_instance> <key> <value>",
         "Add a variable on subsequent deploys"
-      def add(named_instance, key, value); end
+      def add(named_instance, key, value)
+        #TODO: authz
+        Instance.new(named_instance).var_file
+          .add(key, value)
+      end
 
       desc "remove <named_instance> <key>",
         "Cease installing the key to value on subsequent deploys"
-      def remove(named_instance, key); end
+      def remove(named_instance, key)
+        #TODO: authz
+        Instance.new(named_instance).var_file
+          .remove(key)
+      end
     end
 
     class File < Thor
       desc "list <named_instance>", "List the files"
-      def list(named_instance); end
+      def list(named_instance)
+        #TODO: authz
+        Instance.new(named_instance).config_files
+          .list
+      end
 
-      desc "add <named_instance> <app_path> <local_path>",
+      desc "add <named_instance> <app_path>",
         "Add a file to be installed to <app_path> on subsequent deploys"
-      def add(named_instance, app_path, local_path); end
+      def add(named_instance, filename, app_path)
+        #TODO: authz
+        contents = STDIN.gets
+        Instance.new(named_instance).config_files
+          .add(filename, app_path, contents)
+      end
 
       desc "remove <named_instance> <app_path>",
         "Cease installing the file to <app_path> on subsequent deploys"
-      def remove(named_instance, app_path); end
+      def remove(named_instance, app_path)
+        #TODO: authz
+        Instance.new(named_instance).config_files
+          .remove(app_path)
+      end
 
       desc "move <named_instance> <app_path> <new_app_path>",
         "Install the file at <app_path> to <new_app_path> instead on subsequent deploys"
-      def move(named_isntance, app_path, new_app_path); end
+      def move(named_instance, app_path, new_app_path)
+        #TODO: authz
+        Instance.new(named_instance).config_files
+          .move(app_path, new_app_path)
+      end
     end
 
     class Log < Thor
