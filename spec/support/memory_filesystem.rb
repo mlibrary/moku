03445f3a072cdcebc5d4838185632db44ed2bf9f
Bryan Hockey <bhock@umich.edu>
2017-09-06 15:54:05 -0500

    Add MemoryFilesystem for specs

---
+++ spec/support/memory_filesystem.rb
@@ -0,0 +1,28 @@
+require "fauxpaas/filesystem"
+
+module Fauxpaas
+
+  class MemoryFilesystem < Filesystem
+
+    def initialize
+      @files = {}
+    end
+
+    def mkdir_p(path); end
+
+    def write(path, contents)
+      @files[path] = contents
+    end
+
+    def remove(path)
+      @files.delete(path)
+    end
+
+    def read(path)
+      @files[path]
+    end
+
+  end
+
+end
+
