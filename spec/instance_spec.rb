1dbebf0a19cfd1e608e51cf7240aee66f724e365
Bryan Hockey <bhock@umich.edu>
2017-09-26 12:16:34 -0500

    Remove VarFile and ConfigFile tests from Instance

--- spec/instance_spec.rb
+++ spec/instance_spec.rb
@@ -8,18 +8,5 @@ module Fauxpaas
     let(:stage) { "mystage" }
     let(:instance) { described_class.new("#{app}-#{stage}") }
 
-    describe "#var_file" do
-      let(:file_path) { Fauxpaas.instance_root + app + stage + "fauxpaas.yml" }
-      it "returns the VarFile instance with the correct path" do
-        expect(instance.var_file.path).to eql(file_path)
-      end
-    end
-
-    describe "#config_files" do
-      it "returns the ConfigFiles instance with the correct path" do
-        expect(instance.config_files.path).to eql(instance.path)
-      end
-    end
-
   end
 end
