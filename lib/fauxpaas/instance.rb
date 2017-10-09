6ba7d7ab72db2ec26bb3807859850e50280ceed5
Bryan Hockey <bhock@umich.edu>
2017-10-09 14:59:59 -0500

    Add instance.rb

--- lib/fauxpaas/instance.rb
+++ lib/fauxpaas/instance.rb
@@ -5,14 +5,32 @@ module Fauxpaas
   # Represents a named instance within fauxpaas, as opposed
   # to installed on destination servers.
   class Instance
-    attr_reader :app, :stage
-
     def initialize(name)
       @app, @stage = name.split("-")
     end
 
     def path
       Fauxpaas.instance_root + app + stage
+    end
+
+    def name
+      "#{app}-#{stage}"
+    end
+
+    def deploy_config_path
+      path + "deploy_config.yml"
+    end
+
+    def deploy_conf
+      @deploy_conf ||= DeployConfig.new(deploy_config_path)
+    end
+
+    def dev_config_path
+      path + "dev_config"
+    end
+
+    def dev_conf
+      @dev_conf ||= DevConfig.new(dev_config_path)
     end
 
   end
