6fe2e3b95cdfc4004c4fdc04f37e4af31a235ed4
Bryan Hockey <bhock@umich.edu>
2017-10-10 14:56:18 -0500

    Just call cap

--- lib/fauxpaas/cli/main.rb
+++ lib/fauxpaas/cli/main.rb
@@ -8,14 +8,8 @@ module Fauxpaas
 
       desc "deploy <instance>",
       "Deploys the latest revision of the instance's source"
-      def deploy(instance_name, target)
-        instance = InstanceRepo.find(instance_name)
-        DeployedReleaseRepo.create(
-          name: instance.name,
-          deploy_user: instance.deploy_user,
-          release_root: instance.release_root,
-          source: instance.source,
-        )
+      def deploy(instance_name)
+        system("cap #{instance_name} deploy")
       end
     end
 
