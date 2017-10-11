011589cff71b449224934ca590c994cd3d35d9f5
Bryan Hockey <bhock@umich.edu>
2017-10-11 13:25:21 -0500

    Add Fauxpaas.instance_repo

--- lib/fauxpaas.rb
+++ lib/fauxpaas.rb
@@ -1,11 +1,21 @@
 require "fauxpaas/version"
 require "fauxpaas/cli"
 require "fauxpaas/instance"
+require "fauxpaas/file_instance_repo"
 
 module Fauxpaas
 
   class << self
     attr_writer :instance_root
+
+    def root
+      @root ||= Pathname.new(__FILE__).dirname.parent
+    end
+
+    def instance_repo
+      @instance_repo ||= FileInstanceRepo.new(root + "deploy" + "instances")
+    end
+
     def instance_root
       @instance_root ||= Pathname.new(__FILE__).dirname.parent.parent
     end
