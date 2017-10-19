8b14ebdda98760f640e2c7c0ac8c8c4740888c8f
Bryan Hockey <bhock@umich.edu>
2017-10-19 12:53:27 -0500

    Add caches command
    * Add caches command to cli
    * Add caches method to CapistranoDeployer
    * Add a "split token", which we use to artificially introduce strings to
      cap tasks that we use to split output later.
    
    We get the list by simply using cap to perform an ls on the releases
    directory at each machine. The capistrano deployer then parses this
    output into an array of strings and returns that to the cli.
    
    At this stage, we don't do anything special with the directory listing,
    so it is only timestamps of when cap performed the deploy, e.g.
    20171019013839. At a later time when there is a local history to lean
    on, this output can be made more useful.
    
    Finally, we investigating dropping into the cap environment from
    directly within ruby, which would let us avoid the split_token nonsense.
    However, this is not a documented feature of cap/rake, and the path
    to hacking this functionality together was non-trivial and appeared
    unstable.

--- lib/fauxpaas/cli/main.rb
+++ lib/fauxpaas/cli/main.rb
@@ -7,10 +7,17 @@ module Fauxpaas
     class Main < Thor
 
       desc "deploy <instance>",
-      "Deploys the latest revision of the instance's source"
+        "Deploys the latest revision of the instance's source"
       def deploy(instance_name)
         instance = Fauxpaas.instance_repo.find(instance_name)
         Fauxpaas.deployer.deploy(instance)
+      end
+
+      desc "caches <instance>",
+        "List cached releases for the instance"
+      def caches(instance_name)
+        instance = Fauxpaas.instance_repo.find(instance_name)
+        puts Fauxpaas.deployer.caches(instance)
       end
     end
 
--- spec/capistrano_deployer_spec.rb
+++ spec/capistrano_deployer_spec.rb
@@ -4,8 +4,10 @@ require "fauxpaas/capistrano_deployer"
 module Fauxpaas
   RSpec.describe CapistranoDeployer do
 
+    let(:success) { double(:success, success?: true) }
+    let(:failure) { double(:failure, success?: false) }
     let(:path) { "/base/path" }
-    let(:kernel) { double(:kernel, system: nil) }
+    let(:kernel) { double(:kernel, capture3: ["", "", success]) }
 
     let(:instance) do
       double(:instance, name: "myapp-staging", deployer_env: "rails")
@@ -15,9 +17,17 @@ module Fauxpaas
 
     describe "#deploy" do
       it "invokes capistrano" do
-        expect(kernel).to receive(:system)
+        expect(kernel).to receive(:capture3)
           .with("cap -f #{path}/#{instance.deployer_env}.capfile #{instance.name} deploy")
         deployer.deploy(instance)
+      end
+    end
+
+    describe "#caches" do
+      it "invokes capistrano" do
+        expect(kernel).to receive(:capture3)
+          .with("cap -f #{path}/#{instance.deployer_env}.capfile #{instance.name} caches:list")
+        deployer.caches(instance)
       end
     end
 
