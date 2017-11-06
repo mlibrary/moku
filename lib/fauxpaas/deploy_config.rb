e6c74756198c767cf4b5b3a1d24badb406cef80b
Bryan Hockey <bhock@umich.edu>
2017-11-06 12:39:09 -0600

    Add DeployArchive, DeployConfig
    DeployConfig wraps those value pairs that dictate how a deployment
    action should be carried out.
    
    DeployArchive wraps Archive, and extends it to instantiate a
    DeployConfig.

--- lib/fauxpaas/deploy_config.rb
+++ lib/fauxpaas/deploy_config.rb
@@ -1,5 +1,12 @@
+require "active_support/core_ext/hash/keys"
+
 module Fauxpaas
   class DeployConfig
+
+    def self.from_hash(hash)
+      new(hash.symbolize_keys)
+    end
+
     def initialize(deployer_env:, deploy_dir:, rails_env:, assets_prefix:)
       @deployer_env = deployer_env
       @deploy_dir = deploy_dir
@@ -8,6 +15,20 @@ module Fauxpaas
     end
 
     attr_reader :deployer_env, :deploy_dir, :rails_env, :assets_prefix
+
+    def to_hash
+      @hash ||= {
+        deployer_env: deployer_env,
+        deploy_dir: deploy_dir,
+        rails_env: rails_env,
+        assets_prefix: assets_prefix
+      }.stringify_keys
+    end
+
+    def eql?(other)
+      to_hash == other.to_hash
+    end
+    alias_method :==, :eql?
 
 
   end
---
+++ spec/deploy_archive_spec.rb
@@ -0,0 +1,37 @@
+require_relative "./spec_helper"
+require "fauxpaas/deploy_archive"
+require "fauxpaas/deploy_config"
+require "pathname"
+
+module Fauxpaas
+  RSpec.describe DeployArchive do
+    let(:archive) { double(:archive) }
+    let(:reference) { double(:reference) }
+    let(:path) { Pathname.new("some/path") }
+    let(:tmpdir) { Pathname.new("/tmp/dir") }
+    let(:fs) { double(:fs) }
+    let(:deploy_config) do
+      DeployConfig.new(
+        deployer_env: "foo.capfile",
+        assets_prefix: "assets",
+        rails_env: "production",
+        deploy_dir: "/path/to/deploy/dir"
+      )
+    end
+
+    let(:deploy_archive) { described_class.new(archive, root_dir: path, fs: fs) }
+
+    before(:each) do
+      allow(archive).to receive(:checkout).with(reference).and_yield(tmpdir)
+      allow(fs).to receive(:read).with(tmpdir + path + "deploy.yml")
+        .and_return(YAML.dump(deploy_config.to_hash))
+    end
+
+    describe "#deploy_config" do
+      it "builds a deploy_config object from the reference" do
+        expect(deploy_archive.deploy_config(reference))
+          .to eql(deploy_config)
+      end
+    end
+  end
+end
