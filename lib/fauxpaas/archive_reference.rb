bcce54ffc1f1bfef172616e7a729afaafd38ad39
Bryan Hockey <bhock@umich.edu>
2018-01-15 15:56:09 -0600

    Remove root_dir support from ArchiveReference

--- lib/fauxpaas/archive_reference.rb
+++ lib/fauxpaas/archive_reference.rb
@@ -7,13 +7,10 @@ module Fauxpaas
   # A version-control archive at a specific point in time.
   class ArchiveReference
 
-    DEFAULT_DIR = Pathname.new("")
-
     def self.from_hash(hash)
       new(
         hash["url"],
         hash["commitish"],
-        hash.fetch("root_dir", DEFAULT_DIR)
       )
     end
 
@@ -21,25 +18,20 @@ module Fauxpaas
     # to resolve the commitish.
     # @param url [String]
     # @param commitish [String]
-    # @param root_dir [Pathname] The directory at which this archive
-    #   begins. For most repositories, this is the root directory.
     # @return [ArchiveReference]
-    def self.at(url, commitish, root_dir = DEFAULT_DIR)
+    def self.at(url, commitish)
       sha = Fauxpaas.git_runner.sha(url, "#{commitish}^{}")
       sha ||= Fauxpaas.git_runner.sha(url, commitish)
       sha ||= commitish
-      new(url, sha, Pathname.new(root_dir))
+      new(url, sha)
     end
 
     # Create a new instance without resolving the commitish.
     # @param url [String]
     # @param commitish [String]
-    # @param root_dir [Pathname] The directory at which this archive
-    #   begins. For most repositories, this is the root directory.
-    def initialize(url, commitish, root_dir = DEFAULT_DIR)
+    def initialize(url, commitish)
       @url = url
       @commitish = commitish
-      @root_dir = Pathname.new(root_dir)
     end
 
     attr_reader :url, :commitish
@@ -50,7 +42,7 @@ module Fauxpaas
     # a specific tag or SHA is the corresponding SHA itself.
     # @return [ArchiveReference]
     def latest
-      self.class.at(url, commitish, root_dir)
+      self.class.at(url, commitish)
     end
 
     # Get a resolved reference to the commitish.
@@ -59,38 +51,27 @@ module Fauxpaas
     # @return [ArchiveReference]
     def at(new_commitish)
       return latest unless new_commitish
-      self.class.at(url, new_commitish, root_dir)
+      self.class.at(url, new_commitish)
     end
 
     # @yield [GitRunner::WorkingDirectory] The directory in which
     #   the content has been checked out.
     def checkout
       Fauxpaas.git_runner.safe_checkout(url, commitish) do |working_dir|
-        files = working_dir
-          .relative_files
-          .select {|file| file.fnmatch?("#{root_dir}/**") }
-          .map {|file| file.relative_path_from(root_dir) }
-        yield WorkingDirectory.new(working_dir.dir/root_dir, files)
+        yield working_dir
       end
     end
 
     def eql?(other)
-      [:@url, :@commitish, :@root_dir].index do |var|
-        instance_variable_get(var) != other.instance_variable_get(var)
-      end.nil?
+      url == other.url && commitish == other.commitish
     end
 
     def to_hash
       {
         "url"       => url,
         "commitish" => commitish,
-        "root_dir"  => root_dir.to_s
       }
     end
-
-    private
-
-    attr_reader :root_dir
 
   end
 end
