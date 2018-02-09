d0adff6227c02e8438db8a259e4a5171ae3c336c
Bryan Hockey <bhock@umich.edu>
2018-02-09 13:43:31 -0600

    Convert DeployConfig to subclass of OpenStruct

--- lib/fauxpaas/deploy_config.rb
+++ lib/fauxpaas/deploy_config.rb
@@ -2,32 +2,22 @@
 
 require "active_support/core_ext/hash/keys"
 require "fauxpaas/cap"
+require "ostruct"
 
 module Fauxpaas
 
   # The deploy configuration used in the deployment of the instance. I.e. _how_ the
   # instance gets deployed.
-  class DeployConfig
+  class DeployConfig < OpenStruct
 
     def self.from_hash(hash)
       new(hash.symbolize_keys)
     end
 
     def initialize(hash = {})
-      @hash = hash
-      @hash[:systemd_services] ||= []
-    end
-
-    def respond_to?(method)
-      super || hash.has_key?(method)
-    end
-
-    def method_missing(method, *args, &block)
-      if respond_to?(method)
-        hash.fetch(method)
-      else
-        super(method, args, &block)
-      end
+      hash[:systemd_services] ||= []
+      super(hash)
+      self.freeze
     end
 
     def runner
@@ -39,16 +29,8 @@ module Fauxpaas
     end
 
     def to_hash
-      hash.stringify_keys
+      marshal_dump.stringify_keys
     end
-
-    def eql?(other)
-      to_hash == other.to_hash
-    end
-    alias_method :==, :eql?
-
-    private
-    attr_reader :hash
-
+    alias_method :to_h, :to_hash
   end
 end
