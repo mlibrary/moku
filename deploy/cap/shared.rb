mlibrary/moku

commit 69890fa2f1c89f090ec8002f016ffc98cb5e36db

More intelligent lockdown of deployed files
* Switched from using a recursive chmod to using find.
* Don't lock down shared/public
* Don't bother locking shared/bundle

deploy/cap/shared.rb
@@ -21,7 +21,15 @@ namespace :shared do
       Pathname.new(fetch(:shared_local_path)).children.each do |path|
         upload! path.to_s, fetch(:shared_remote_path), recursive: path.directory?
       end
-      execute :chmod, "-R", "go-rw", fetch(:shared_remote_path)
+
+      # Lock down files, except for public. Don't waste time on the bundled gems.
+      execute :find, fetch(:shared_remote_path), %Q(
+        -path #{fetch(:shared_remote_path)}/public -prune -o
+        -path #{fetch(:shared_remote_path)}/bundle -prune -o
+        -type f
+        -perm /g=rw,o=rw
+        -exec chmod go-rwx '{}' \;"
+      )
     end
   end
 

deploy/cap/unshared.rb
@@ -13,7 +13,14 @@ namespace :unshared do
       Pathname.new(fetch(:unshared_local_path)).children.each do |path|
         upload! path.to_s, fetch(:release_path), recursive: path.directory?
       end
-      execute :chmod, "-R", "go-rw", fetch(:release_path)
+
+      # Lock down files, but dont follow symlinks into shared dir
+      execute :find, fetch(:release_path), %Q(
+        -P
+        -type f
+        -perm /g=rw,o=rw
+        -exec chmod go-rwx '{}' \;
+      )
     end
   end
 end

commit e04956696cd8699875ebfac957f8071d0103ab56

Fix fatal bug that occurs when locking files
This bug was introduced in 69890fa. The logic of each of the find
commands was correct, but the syntax was wrong. This fixes that
issue.

deploy/cap/shared.rb
@@ -23,12 +23,12 @@ namespace :shared do
       end
 
       # Lock down files, except for public. Don't waste time on the bundled gems.
-      execute :find, fetch(:shared_remote_path), %Q(
+      execute :find, fetch(:shared_remote_path), %W(
         -path #{fetch(:shared_remote_path)}/public -prune -o
         -path #{fetch(:shared_remote_path)}/bundle -prune -o
         -type f
         -perm /g=rw,o=rw
-        -exec chmod go-rwx '{}' \;"
+        -exec chmod go-rwx '{}' \\;
       )
     end
   end

deploy/cap/unshared.rb
@@ -15,11 +15,10 @@ namespace :unshared do
       end
 
       # Lock down files, but dont follow symlinks into shared dir
-      execute :find, fetch(:release_path), %Q(
-        -P
+      execute :find, "-P", fetch(:release_path), %W(
         -type f
         -perm /g=rw,o=rw
-        -exec chmod go-rwx '{}' \;
+        -exec chmod go-rwx '{}' \\;
       )
     end
   end