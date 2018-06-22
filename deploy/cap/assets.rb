ae9508ca13f7e40ea287ed50bb46a437071e07db
Bryan Hockey <bhock@umich.edu>
2018-06-22 15:54:23 -0500

    Fix error on missing assets:precompile task
    Resolves AEIM-1270
    
    The task set to run after assets:precompile (defined in
    deploy/cap/assets.rb) would cause an _immediate_ error as the
    assets:precompile task was not defined.
    
    By adding an empty task, this error condition is removed. The dummy task
    does not override the actual task regardless of definition order.

--- deploy/cap/assets.rb
+++ deploy/cap/assets.rb
@@ -1,5 +1,6 @@
 namespace :deploy do
   namespace :assets do
+    task :precompile
     after :precompile, :open_permissions do
       on release_roles(fetch(:assets_roles)) do
         execute(:chmod, "-R", "go+r", File.join(fetch(:deploy_to), "shared", "public", "assets"))
