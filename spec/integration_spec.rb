d57fa41f9dcb599c63af81acf59f59b5c48d9654
Bryan Hockey <bhock@umich.edu>
2018-06-26 16:49:28 -0500

    Add debug output in integration tests via envvar

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -25,7 +25,12 @@ module Fauxpaas
             config.register(:deployer_env_root) do
               Pathname.new("spec/fixtures/integration/capfiles").expand_path(Fauxpaas.root)
             end
-            config.register(:logger) { Logger.new(StringIO.new, level: :info) }
+            if ENV["DEBUG"]
+              config.register(:logger) { Logger.new(STDOUT, level: :debug) }
+              config.register(:system_runner) { Fauxpaas::PassthroughRunner.new(STDOUT) }
+            else
+              config.register(:logger) { Logger.new(StringIO.new, level: :info) }
+            end
           end
           Fauxpaas.invoker.add_command(
             DeployCommand.new(
