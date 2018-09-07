f52baad27c950ce4e617fdf83b3d9258abc4e1cc
Noah Botimer <botimer@umich.edu>
2018-09-06 23:04:36 -0400

    AEIM-1519 - Remove platform dependency of PassthroughRunner
    The tests for PassthroughRunner check for platform-specific output of
    the ls command on a nonexistent file as a way to get output to stderr.
    Here, we change to use standard redirection and fixed output with echo.
    
    Because the implementation always merges stdout and stderr, this also
    changes the examples for separate streams to exclude output to the
    other stream and marks those examples as pending. They could be enabled
    if/when separate streams were available (maybe via an option).

--- spec/passthrough_runner_spec.rb
+++ spec/passthrough_runner_spec.rb
@@ -9,31 +9,25 @@ module Fauxpaas
     let(:runner) { described_class.new(log) }
     describe "#run" do
       it "returns a status object" do
-        _, _, status= runner.run("echo foo")
+        _, _, status = runner.run("echo foo")
         expect(status).to be_a_kind_of(Process::Status)
         expect(status.success?).to be true
       end
 
       it "prints stdout and stderr together" do
-        runner.run("echo foo; ls nofile")
+        runner.run("echo output; echo error >&2")
         log.rewind
-        expect(log.read).to match(
-          /foo\nls: cannot access '?nofile'?: No such file or directory\n/
-        )
+        expect(log.read).to match(/output\nerror/)
       end
 
-      it "returns stdout" do
-        stdout, _, _ = runner.run("echo foo; ls nofile")
-        expect(stdout).to match(
-          /foo\nls: cannot access '?nofile'?: No such file or directory\n/
-        )
+      xit "returns stdout", pending: 'We always merge stdout and stderr for now; this could be enabled if PassthroughRunner gets a merge option' do
+        stdout, _, _ = runner.run("echo output; echo error >&2")
+        expect(stdout).not_to match(/error/)
       end
 
-      it "returns stderr" do
-        _, stderr, _ = runner.run("echo foo; ls nofile")
-        expect(stderr).to match(
-          /foo\nls: cannot access '?nofile'?: No such file or directory\n/
-        )
+      xit "returns stderr", pending: 'We always merge stdout and stderr for now; this could be enabled if PassthroughRunner gets a merge option' do
+        _, stderr, _ = runner.run("echo output; echo error >&2")
+        expect(stderr).not_to match(/output/)
       end
 
       it "prints output as it happens"
