6a0afaca39c7428d164d1638908f5d46def85da4
Bryan Hockey <bhock@umich.edu>
2018-12-19 17:36:51 -0600

    Fix contract in Task::Task#call

--- lib/moku/task/task.rb
+++ lib/moku/task/task.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require "moku/status"
+
 module Moku
   module Task
 
@@ -8,7 +10,9 @@ module Moku
     class Task
       # @param target [Artifact,Release]
       # @return [Status]
-      def call(target); end
+      def call(target)
+        Status.success
+      end
 
       def to_s
         self.class.to_s
