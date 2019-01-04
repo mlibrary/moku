6557d651107550b6c81ec693252d5e993a88e0b3
Bryan Hockey <bhock@umich.edu>
2019-01-04 12:18:20 -0600

    Fix setting rbenv env in finish_release in test

--- lib/moku/release.rb
+++ lib/moku/release.rb
@@ -51,6 +51,20 @@ module Moku
 
     attr_reader :artifact, :deploy_config, :remote_runner, :user
 
+    # Environment manipulation necessary to adopt the rbenv version of the
+    # source to be installed. This only has an effect in the test environment
+    # and development enviroments.
+    def rbenv_env
+      "PATH=$RBENV_ROOT/versions/$(rbenv local)/bin:$PATH"
+    end
+
+    def contextualize(command)
+      "if [ -d #{deploy_path} ]; " \
+        "then cd #{deploy_path}; " \
+        "fi; " \
+        "#{rbenv_env} #{deploy_config.shell_env} #{command}"
+    end
+
     def run_on_hosts(hosts, command)
       Sequence.for(hosts) do |host|
         remote_runner.run(
@@ -59,13 +73,6 @@ module Moku
           command: contextualize(command)
         )
       end
-    end
-
-    def contextualize(command)
-      "if [ -d #{deploy_path} ]; " \
-        "then cd #{deploy_path}; " \
-        "fi; " \
-        "#{deploy_config.shell_env} #{command}"
     end
 
   end
