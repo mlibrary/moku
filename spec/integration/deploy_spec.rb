00f5b09bcb6bc17f287ef053d4adcaf01bd8971b
Bryan Hockey <bhock@umich.edu>
2019-01-08 14:22:08 -0600

    Format spec/integration/deploy_spec.rb

--- spec/integration/deploy_spec.rb
+++ spec/integration/deploy_spec.rb
@@ -11,61 +11,59 @@ require "pathname"
 
 module Moku
 
-  RSpec.describe "deploy integration", integration: true do
-    describe "deploy" do
-      context "without rails" do
-        include_context "with a sandbox", "test-norails"
-        include_context "with a deployed instance", "test-norails"
-        let(:gem) { "pry" }
-        let(:development_gem) { "faker" }
-        let(:test_gem) { "rspec" }
-        let(:source) { Pathname.new("some_source_file.txt") }
+  RSpec.describe "integration deploy", integration: true do
+    context "without rails" do
+      include_context "with a sandbox", "test-norails"
+      include_context "with a deployed instance", "test-norails"
+      let(:gem) { "pry" }
+      let(:development_gem) { "faker" }
+      let(:test_gem) { "rspec" }
+      let(:source) { Pathname.new("some_source_file.txt") }
 
-        it_behaves_like "a successful deploy"
+      it_behaves_like "a successful deploy"
 
-        it "doesn't install development gems" do
-          within(current_dir) do |env|
-            expect(`#{env} bundle list`).not_to match(/#{development_gem}/)
-          end
-        end
-
-        it "doesn't install test gems" do
-          within(current_dir) do |env|
-            expect(`#{env} bundle list`).not_to match(/#{test_gem}/)
-          end
+      it "doesn't install development gems" do
+        within(current_dir) do |env|
+          expect(`#{env} bundle list`).not_to match(/#{development_gem}/)
         end
       end
 
-      context "with rails" do
-        include_context "with a sandbox", "test-rails"
-        include_context "with a deployed instance", "test-rails"
-        let(:gem) { "rails" }
-        let(:source) { Pathname.new("config/environment.rb") }
-
-        it_behaves_like "a successful deploy"
-
-        it "current/public/assets 2775" do
-          expect(current_dir/"public"/"assets").to have_permissions("2775")
+      it "doesn't install test gems" do
+        within(current_dir) do |env|
+          expect(`#{env} bundle list`).not_to match(/#{test_gem}/)
         end
+      end
+    end
 
-        it "current/bin/rails 6770" do
-          expect(current_dir/"bin"/"rails").to have_permissions("6770")
-        end
+    context "with rails" do
+      include_context "with a sandbox", "test-rails"
+      include_context "with a deployed instance", "test-rails"
+      let(:gem) { "rails" }
+      let(:source) { Pathname.new("config/environment.rb") }
 
-        it "runs the migrations" do
-          expect(`sqlite3 #{current_dir/"db"/"production.sqlite3"} ".schema posts"`.chomp)
-            .to match(/CREATE TABLE (IF NOT EXISTS )?"posts"/)
-          expect(`sqlite3 #{current_dir/"db"/"production.sqlite3"} ".schema things"`.chomp)
-            .to match(/CREATE TABLE (IF NOT EXISTS )?"things"/)
-        end
+      it_behaves_like "a successful deploy"
 
-        it "installs a working project" do
-          within(current_dir) do |env|
-            _, _, status = Open3.capture3(
-              "#{env} bin/rails runner -e production 'Post.new.valid?'"
-            )
-            expect(status.success?).to be true
-          end
+      it "current/public/assets 2775" do
+        expect(current_dir/"public"/"assets").to have_permissions("2775")
+      end
+
+      it "current/bin/rails 6770" do
+        expect(current_dir/"bin"/"rails").to have_permissions("6770")
+      end
+
+      it "runs the migrations" do
+        expect(`sqlite3 #{current_dir/"db"/"production.sqlite3"} ".schema posts"`.chomp)
+          .to match(/CREATE TABLE (IF NOT EXISTS )?"posts"/)
+        expect(`sqlite3 #{current_dir/"db"/"production.sqlite3"} ".schema things"`.chomp)
+          .to match(/CREATE TABLE (IF NOT EXISTS )?"things"/)
+      end
+
+      it "installs a working project" do
+        within(current_dir) do |env|
+          _, _, status = Open3.capture3(
+            "#{env} bin/rails runner -e production 'Post.new.valid?'"
+          )
+          expect(status.success?).to be true
         end
       end
     end
