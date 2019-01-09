1f273a9cd5f6cfc38f469a7f01aa3d42cd5fca96
Bryan Hockey <bhock@umich.edu>
2019-01-08 18:53:48 -0600

    Document examples for Sequence

--- lib/moku/sequence.rb
+++ lib/moku/sequence.rb
@@ -13,6 +13,9 @@ module Moku
     # either the collection is exhausted or one of the items returns a failing
     # Status object. If no failures are encountered, this returns a successful
     # Status object. Otherwise, it returns the failed status object.
+    #
+    # @example Pass a release to each task's #call method
+    #   Sequence.do(tasks, release)
     def self.do(items, *args)
       items.reduce(Status.success) do |last_result, item|
         break(last_result) unless last_result.success?
@@ -25,6 +28,14 @@ module Moku
     # either the collection is exhausted or one of the items returns a failing
     # Status object. If no failures are encountered, this returns a successful
     # Status object. Otherwise, it returns a failed status object.
+    #
+    # Note that your block must return something that responds to #success?,
+    # such as a Status instance.
+    #
+    # @example
+    #   Sequence.for(commands) do |command|
+    #     runner.run(command)
+    #   end
     def self.for(items)
       items.reduce(OpenStruct.new(success?: true)) do |last_result, item|
         break(last_result) unless last_result.success?
