47bc4ddb0ee78f1e1b0b93863e46ead37e3175a2
Bryan Hockey <bhock@umich.edu>
2019-01-22 16:35:17 -0600

    Add missing requires in command.rb

--- lib/moku/command.rb
+++ lib/moku/command.rb
@@ -1,9 +1,12 @@
 # frozen_string_literal: true
 
 require "moku/command/command"
+require "moku/command/caches"
 require "moku/command/deploy"
+require "moku/command/exec"
 require "moku/command/read_default_branch"
 require "moku/command/releases"
+require "moku/command/rollback"
 require "moku/command/set_default_branch"
 
 module Moku
