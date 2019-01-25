c3dfd7c01036383a8f585bfa96464dead1116e44
Bryan Hockey <bhock@umich.edu>
2019-01-25 17:25:13 -0600

    Fix failing Task::Task#to_s test

--- spec/task/task_spec.rb
+++ spec/task/task_spec.rb
@@ -7,7 +7,7 @@ module Moku
   RSpec.describe Task::Task do
     describe "#to_s" do
       it "returns the class name" do
-        expect(described_class.new.to_s).to eql(described_class.to_s)
+        expect(described_class.new.to_s).to eql(described_class.to_s.split("::").last)
       end
     end
   end
